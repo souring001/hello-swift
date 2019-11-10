7fac1da332f0f43015bb43a71227bd3f6e222cd5
aso <titechei@gmail.com>
2019-11-10 12:24:50 +0900

    Hello Swift

--- MyFirst/ViewController.swift
+++ MyFirst/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        outputLabel.text = "Hello Swift!"
     }
 
-
+    @IBOutlet weak var outputLabel: UILabel!
+    
 }
 
