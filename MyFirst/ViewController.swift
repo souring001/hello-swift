cf609bee492be2ef506f81976fee5f618b4f163f
aso <titechei@gmail.com>
2019-11-10 12:10:52 +0900

    Initial Commit

---
+++ MyFirst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirst
+//
+//  Created by mechapony on 2019/11/10.
+//  Copyright © 2019 mechapony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
